5e541bbbd437c1ba2e067d69805b4e8af8f8af48
lch-howger <360000521@qq.com>
2020-04-22 23:59:32 +0100

    update Base

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        BorderPane borderPane = new BorderPane();
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(borderPane, 800, 600));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
